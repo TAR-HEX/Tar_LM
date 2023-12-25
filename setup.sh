6645837cec932b946b19f062d7e1334ea8077e78
TARHEXX <154792084+TARHEXX@users.noreply.github.com>
2023-12-25 19:44:46 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 # Banner
 printf " _____          _   _ \n"
 printf "|_   _|_ _ _ __| | | | _____  __\n"
@@ -19,5 +21,6 @@ pkg install python -y
 pip install re
 pip install requests
 pip install bs4
+clear
 
 python3 linkphish.py
